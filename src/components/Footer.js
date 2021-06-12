25df4f06ab69cf631fe835b2bb7fded33a83ff81
yuji oda <ug0416.33402@gmail.com>
2021-06-12 17:30:29 +0900

    change layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
+import footerlogo from '../img/footer_logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -13,7 +13,7 @@ const Footer = class extends React.Component {
       <footer className="footer has-background-black has-text-white-ter">
         <div className="content has-text-centered">
           <img
-            src={logo}
+            src={footerlogo}
             alt="Kaldi"
             style={{ width: '14em', height: '10em' }}
           />
