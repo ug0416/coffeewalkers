c4dbd961a5eaaf3d75ad10cd7b38b37b45c09a04
yuji oda <ug0416.33402@gmail.com>
2021-06-17 22:48:57 +0900

    change tags

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -11,7 +11,7 @@ class TagRoute extends React.Component {
       <div className="columns is-multiline">
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
+            <div className="is-parent column is-6" key={post.node.fields.slug}>
               <article
                 className={`blog-list-item tile is-child box notification ${
                   post.frontmatter.featuredpost ? 'is-featured' : ''
